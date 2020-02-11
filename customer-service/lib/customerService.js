6bbe2f4e7bccce0ade123a8b34e4513c52e7f9f1
dartvandru <valentin.andruschenko@diritalarrowtech.com>
2020-02-11 22:19:00 +0200

    Wrapped create customer into transaction

--- customer-service/lib/customerService.js
+++ customer-service/lib/customerService.js
@@ -4,6 +4,7 @@ const MessageProducer = require('eventuate-tram-core-nodejs/lib/MessageProducer'
 const Customer = require('./aggregates/Customer');
 const { CustomerEntityTypeName } = require('../../common/eventsConfig');
 const { insertIntoCustomerTable } = require('./mysql/customerCrudService');
+const knex = require('./mysql/knex');
 
 const channelMapping = new DefaultChannelMapping(new Map());
 const messageProducer = new MessageProducer({ channelMapping });
@@ -11,12 +12,17 @@ const domainEventPublisher = new DomainEventPublisher({ messageProducer });
 
 module.exports.create = async ({ name, creditLimit }) => {
   const creationTime = new Date().getTime();
+  const trx = await knex.transaction();
+  try {
+    const [ aggregateId ] = await insertIntoCustomerTable(name, creditLimit.amount, creationTime, { trx });
 
-  const [ aggregateId ] = await insertIntoCustomerTable(name, creditLimit.amount, creationTime);
-
-  const customerAndEvents = Customer.create({ name, creditLimit });
-  const aggregateType = CustomerEntityTypeName;
-  const extraHeaders = [];
-  await domainEventPublisher.publish(aggregateType, aggregateId, extraHeaders, customerAndEvents.events);
-  return aggregateId;
+    const customerAndEvents = Customer.create({ name, creditLimit });
+    const aggregateType = CustomerEntityTypeName;
+    const extraHeaders = [];
+    await domainEventPublisher.publish(aggregateType, aggregateId, extraHeaders, customerAndEvents.events, trx);
+    await trx.commit();
+    return aggregateId;
+  } catch (e) {
+    await trx.rollback()
+  }
 };
