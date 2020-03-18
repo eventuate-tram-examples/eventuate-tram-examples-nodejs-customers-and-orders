348fa1a057c95fce02df14854b937cc6e61a3f7f
dartvandru <valentin.andruschenko@diritalarrowtech.com>
2020-03-18 22:34:32 +0200

    Added eventuate images versions into docker-compose.yml

--- run-services.sh
+++ run-services.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-#. ./set-env.sh
-
-export EVENTUATE_COMMON_VERSION=0.8.0.RELEASE
-export EVENTUATE_KAFKA_VERSION=0.3.0.RELEASE
-export EVENTUATE_CDC_VERSION=0.6.0.RC1
-export EVENTUATE_CDC_KAFKA_ENABLE_BATCH_PROCESSING=false
-
 docker-compose up -d zookeeper kafka mysql mongodb cdcservice
 ./wait-for-services.sh $DOCKER_HOST_IP "8099"
-docker-compose up -d customerservice orderhistoryservice orderservice
+docker-compose up -d customer-service order-history-service order-service
 ./wait-for-services.sh $DOCKER_HOST_IP "8081 8082 8083"
