8c19f208439e2dfc9c5a9221efae6cd36a299993
Chris Richardson <chris@chrisrichardson.net>
2020-03-21 12:31:48 -0700

    Moved end to end tests into subdirectory

--- run-end-to-end-tests.sh
+++ run-end-to-end-tests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
-docker run -i --rm -v $(pwd):/tests -e DOCKER_HOST_IP node:12.16.1 bash <<END
-cd /tests
+docker run -i --rm -v $(pwd)/end-to-end-tests:/end-to-end-tests -e DOCKER_HOST_IP node:12.16.1 bash <<END
+cd /end-to-end-tests
 npm install
 npm run test:end-to-end
 END
