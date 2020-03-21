f7f989ee7dc910992ad9b00eabaebfb3f608c996
Chris Richardson <chris@chrisrichardson.net>
2020-03-21 12:22:03 -0700

    Cleaned up CircleCI config

--- run-end-to-end-tests.sh
+++ run-end-to-end-tests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
-docker run -i --rm -v $(pwd):/stuff -e DOCKER_HOST_IP node:12.16.1 bash <<END
-cd /stuff
+docker run -i --rm -v $(pwd):/tests -e DOCKER_HOST_IP node:12.16.1 bash <<END
+cd /tests
 npm install
 npm run test:end-to-end
 END
